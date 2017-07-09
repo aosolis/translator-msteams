d4b8f9500b90e8287d1af4299e4d67a2c2a18e49
Adrian Solis <aosolis@microsoft.com>
2017-07-08 17:49:07 -0700

    Copy initial code from Trello bot.

---
+++ src/TranslatorBot.ts
@@ -0,0 +1,46 @@
+import * as builder from "botbuilder";
+import * as msteams from "botbuilder-teams";
+
+// =========================================================
+// Bot Setup
+// =========================================================
+
+export class TranslatorBot extends builder.UniversalBot {
+
+    private loadSessionAsync: {(address: builder.IAddress): Promise<builder.Session>};
+
+    constructor(
+        public _connector: builder.IConnector,
+        private botSettings: any,
+    )
+    {
+        super(_connector, botSettings);
+        this.set("persistConversationData", true);
+
+        // Handle invoke events
+        this.loadSessionAsync = (address) => {
+            return new Promise((resolve, reject) => {
+                this.loadSession(address, (err: any, session: builder.Session) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(session);
+                    }
+                });
+            });
+        };
+
+        let teamsConnector = this._connector as msteams.TeamsChatConnector;
+        if (teamsConnector.onQuery) {
+            teamsConnector.onQuery("search", (event, query, cb) => { this.onComposeExtensionQuery(event, query, cb); });
+        }
+    }
+
+    // Handle compose extension query invocation
+    private async onComposeExtensionQuery(event: builder.IEvent, query: msteams.ComposeExtensionQuery, cb: (err: Error, result: msteams.IComposeExtensionResponse, statusCode?: number) => void): Promise<void> {
+        let session = await this.loadSessionAsync(event.address);
+        if (session) {
+            cb(new Error(), null, 500);
+        }
+    }
+}
